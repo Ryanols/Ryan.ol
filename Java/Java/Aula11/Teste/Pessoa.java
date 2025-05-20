20f8ffa97b08c6f63cfa6bb646bac9f2c1726cde
Ryan <156459580+Ryanols@users.noreply.github.com>
2025-05-20 20:37:03 -0300

    Delete Java/Java directory

--- Java/Java/Aula11/Teste/Pessoa.java
+++
@@ -1,8 +0,0 @@
-package Aula11.Teste;
-
-public class Pessoa {
-    public String nomeComp;
-    public String profissao;
-    public int idade;
-
-}
--- Java/Java/Aula6/Main.java
+++
@@ -1,8 +0,0 @@
-package Aula6;
-
-public class Main {
-    public static void main (String[] args) {
-        //Escopo das variáveis
-
-    }
-}
